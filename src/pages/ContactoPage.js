6e1cc86d1be60f5df2aa350a7cb64a0d641cd7b9
Flavia Ursino <flavia.ursino@gmail.com>
2024-07-19 12:54:12 -0300

    archivos

--- src/pages/ContactoPage.js
+++ src/pages/ContactoPage.js
@@ -41,7 +41,7 @@ const ContactoPage = (props) => {
             </ul>
             <div className="mapa">
                 <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.2192342946532!2d-58.42268788758324!3d-34.598617372843336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca62d4cf1563%3A0x686de505d84c67e6!2sAv.%20Medrano%20951%2C%20C1179AAQ%20Cdad.%20Aut%C3%B3noma%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1716212973998!5m2!1ses-419!2sar" style={{ border: 0, width:500, height:300 }} >
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.2192342946532!2d-58.42268788758324!3d-34.598617372843336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca62d4cf1563%3A0x686de505d84c67e6!2sAv.%20Medrano%20951%2C%20C1179AAQ%20Cdad.%20Aut%C3%B3noma%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1716212973998!5m2!1ses-419!2sar" style={{ border: 0, width:500, height:300 }} title='mapa' >
                             
                         </iframe>
 
